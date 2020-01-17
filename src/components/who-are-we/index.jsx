98879e6842c49cd8170c80fead65d92d980a10dd
luzzif <federico.luzzi@mondora.com>
2020-01-17 11:47:45 +0100

    chore: copywrite changes

--- src/components/who-are-we/index.jsx
+++ src/components/who-are-we/index.jsx
@@ -14,10 +14,19 @@ export const WhoAreWe = () => (
             <h2>Chi siamo</h2>
         </Box>
         <Box width={["80%", "70%", "60%", "40%"]}>
-            Castellana s.r.l. è un’azienda di famiglia che opera nel settore
-            della distribuzione food & beverage, distribuendo in Italia e
-            all’estero le eccellenze del mercato nazionale ed internazionale
-            sulle tavole dei nostri clienti.
+            <p>
+                Castellana s.r.l. è un’azienda di famiglia che opera nel settore
+                della distribuzione food & beverage, distribuendo in Italia e
+                all’estero le eccellenze del mercato nazionale ed internazionale
+                sulle tavole dei nostri clienti.
+            </p>
+            <p>
+                Con più di 4800 articoli sempre disponibili e conservati in
+                un'area di 7000mq totali, di cui 1000 di celle figorifere,
+                trasportati dalla flotta dei nostri 15 furgoni attrezzati e con
+                le nostre certificazioni in ambito organizzativo non temiamo
+                rivali.
+            </p>
         </Box>
     </Flex>
 );

6938c5cc9bb342d23012dfd4824291a8991a8103
luzzif <federico.luzzi@mondora.com>
2020-01-20 22:22:19 +0100

    fix: fix gatsby production build

--- src/components/where-we-are/index.jsx
+++ src/components/where-we-are/index.jsx
@@ -1,6 +1,5 @@
 import React from "react";
-import { Marker, TileLayer } from "react-leaflet";
-import { StyledMap, StyledPopup } from "./styled";
+import { Map, Marker, Popup, TileLayer } from "react-leaflet";
 
 const LATITUDE = 40.49992;
 const LONGITUDE = 17.3826;
@@ -9,15 +8,22 @@ const ZOOM = 14;
 export const WhereAreWe = () => {
     if (typeof window !== "undefined") {
         return (
-            <StyledMap center={[LATITUDE, LONGITUDE]} zoom={ZOOM}>
+            <Map
+                center={[LATITUDE, LONGITUDE]}
+                zoom={ZOOM}
+                style={{
+                    height: 400,
+                    boxShadow: "0px 10px 42px 0px rgba(0, 0, 0, 0.4)"
+                }}
+            >
                 <TileLayer
                     url="http://{s}.tile.osm.org/{z}/{x}/{y}.png"
                     attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                 />
                 <Marker position={[LATITUDE, LONGITUDE]}>
-                    <StyledPopup>Castellana s.r.l</StyledPopup>
+                    <Popup>Castellana s.r.l</Popup>
                 </Marker>
-            </StyledMap>
+            </Map>
         );
     }
     return null;

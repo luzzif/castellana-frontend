6a3389d3017ab1c3b9db57da8d4ff77a9cede1ac
luzzif <federico.luzzi@mondora.com>
2020-01-20 21:26:44 +0100

    chore: copywrite changes in maps popup

--- src/components/where-we-are/index.jsx
+++ src/components/where-we-are/index.jsx
@@ -1,10 +1,10 @@
 import React from "react";
-import { Marker, Popup, TileLayer } from "react-leaflet";
-import { StyledMap } from "./styled";
+import { Marker, TileLayer } from "react-leaflet";
+import { StyledMap, StyledPopup } from "./styled";
 
 const LATITUDE = 40.49992;
 const LONGITUDE = 17.3826;
-const ZOOM = 12;
+const ZOOM = 14;
 
 export const WhereAreWe = () => (
     <StyledMap center={[LATITUDE, LONGITUDE]} zoom={ZOOM}>
@@ -13,11 +13,7 @@ export const WhereAreWe = () => (
             attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         />
         <Marker position={[LATITUDE, LONGITUDE]}>
-            <Popup>
-                A pretty CSS3 popup.
-                <br />
-                Easily customizable.
-            </Popup>
+            <StyledPopup>Castellana s.r.l</StyledPopup>
         </Marker>
     </StyledMap>
 );
--- src/components/where-we-are/styled.js
+++ src/components/where-we-are/styled.js
@@ -1,7 +1,12 @@
 import styled from "styled-components";
-import { Map } from "react-leaflet";
+import { Map, Popup, Marker } from "react-leaflet";
 
 export const StyledMap = styled(Map)`
     height: 400px;
     box-shadow: 0px 10px 42px 0px rgba(0, 0, 0, 0.4);
+`;
+
+export const StyledPopup = styled(Popup)`
+    font-family: Muli;
+    font-size: 20px;
 `;

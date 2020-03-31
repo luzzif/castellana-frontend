be6a09cbfaeacb8b4605a310fb76c7457623e39b
luzzif <federico.luzzi@mondora.com>
2020-03-31 18:13:27 +0200

    feat: add cookies provider

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,6 +1,13 @@
+import React from "react";
+import { CookiesProvider } from "react-cookie";
+
 if (typeof window !== "undefined") {
     require("smooth-scroll")('a[href*="#"]', {
         easing: "easeInOutQuad",
         offset: 100
     });
 }
+
+export const wrapRootElement = ({ element }) => (
+    <CookiesProvider>{element}</CookiesProvider>
+);
